2716856c1015ce9cea4058184fc7039014a17112
Christian Kelly <chrskly@chrskly.com>
2017-05-08 16:32:27 +0000

    Fix whitespace

--- server/model.py
+++ server/model.py
@@ -4,4 +4,4 @@ import web
 db = web.database(dbn='mysql', db='hab', user='hab', pw='habpass')
 
 def get_latest_telemetry():
-  return db.select('telemetry', order='dt DESC', limit=1)
+    return db.select('telemetry', order='dt DESC', limit=1)
