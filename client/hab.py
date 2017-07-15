f1793e48d6383b14452380d76a77b3296f39b27e
Christian Kelly <chrskly@chrskly.com>
2017-07-15 21:54:30 +0000

    upload barometer data

--- client/hab.py
+++ client/hab.py
@@ -26,24 +26,38 @@ class hab:
         self.current_heading = None
         # current GPS data (json returned from sim.get_gps())
         self.current_gps = None
+        # current baromoeter data
+        self.current_barom = {}
 
-    def get_heading(self):
-        # Get the heading from the magnetometer
+    def get_magnetomoeter_reading(self):
+        """
+        Get the heading from the magnetometer
+        """
         self.curent_heading = self.magnetometer.degrees(self.magnetometer.heading())
         logging.info("heading : %s", self.current_heading)
         self.store.store_heading(self.current_heading[0], self.current_heading[1])
 
-    def get_altitude(self):
+    def get_barometer_reading(self):
+        """
+        Get the temperature and pressure from the barometer.
+        """
         (pressure, temperature) = self.barometer.read_temperature_and_pressure()
         logging.info("barom temp : %s", temperature)
         logging.info("barom pres : %s", pressure)
+        # Keep latest readings in dict
+        self.current_barom['temperature'] = temperature
+        self.current_barom['pressure'] = pressure
+        # Also store readings in local db
         self.store.store_temperature(temperature)
         self.store.store_pressure(pressure)
 
-    def get_location(self):
+    def get_telemetry(self):
         self.current_gps = self.sim.get_gps()
 
     def upload_telemetry(self):
+        """
+        Sends msg to server : GPS,<long>,<lat>,<alt>,<sog>,<cog>
+        """
         logging.info("uploading telemetry")
         if not self.current_gps:
             logging.info("WARNING : no GPS data. Skipping telemetry upload.")
@@ -56,12 +70,34 @@ class hab:
              self.current_gps['speed_over_ground'],
              self.current_gps['course_over_ground']))
 
+    def upload_barometer_reading(self):
+        """
+        Send msg to server : BAROM,<temp>,<pressure>
+        """
+        logging.info("uploading barometer data")
+        if not self.current_barom:
+            logging.info("WARNING : no barometer data. Skipping barometer data upload.")
+            return True
+        # BAROM,<temp>,<pressure>
+        self.sim.data_upload("BAROM,%s,%s" % (self.current_barom['temperature'], self.current_barom['pressure']))
+
+    def main_loop(self):
+        """
+        Do all the things.
+        """
+        self.sim.enable_gps()
+        self.sim.enable_gsm()
+        self.sim.enable_gprs()
+        logging.info("LOCK STATUS : %s" % self.sim.gps_has_lock())
+        # poll GPS
+        self.get_telemetry()
+        print self.upload_telemetry()
+        # poll barometer
+        self.get_barometer_reading()
+        print self.upload_barometer_reading()
 
 
 if __name__ == "__main__":
     myhab = hab()
-    myhab.sim.enable_gps()
-    logging.info("LOCK STATUS : %s" % myhab.sim.gps_has_lock())
-    #myhab.get_location()
-    #print myhab.upload_telemetry()
+    myhab.main_loop()
 
